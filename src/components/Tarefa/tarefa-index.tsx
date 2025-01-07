0aa271b5172eb8b1fc98da6fb1955486d110b5ad
AndreDG88 <andre.soares.designer@gmail.com>
2025-01-07 13:18:53 -0300

    configuração da função edição do projeto

--- src/components/Tarefa/tarefa-index.tsx
+++ src/components/Tarefa/tarefa-index.tsx
@@ -1,17 +1,38 @@
 //importação dos estilos configurados em tarefa-styles.tsx.
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { useDispatch } from 'react-redux'
 import * as S from './tarefa-styles'
-import { remover } from '../../store/reducers/tarefas-reducer'
+import { remover, editar } from '../../store/reducers/tarefas-reducer'
 import TarefaClass from '../../models/Tarefa-model'
 
 // Definição das props dos elementos editáveis, baseadas na clase tarefa em models.
 type Props = TarefaClass
 
 //configuração dos cards descritivos das tarefas.
-const Tarefa = ({ titulo, prioridade, status, descricao, id }: Props) => {
+const Tarefa = ({
+  titulo,
+  prioridade,
+  status,
+  descricao: descricaoOriginal,
+  id
+}: Props) => {
   const dispatch = useDispatch()
   const [estaEditando, setEstaEditando] = useState(false) // Função useState para a alteração entre os botões editar e cancelar
+  const [descricao, setDescricao] = useState('') // Função para receber um valor do textarea de descrição
+
+  // Config de UseEffect para manter as descrições dos cards, caso eles já tenham sido preenchidos.
+  useEffect(() => {
+    if (descricaoOriginal.length > 0) {
+      setDescricao(descricaoOriginal)
+    }
+  }, [descricaoOriginal])
+
+  //Função para que a descrição original volte a caixa de texto quando o botão cancelar for acionado.
+  function cancelarEdicao() {
+    setEstaEditando(false)
+    setDescricao(descricaoOriginal)
+  }
+
   return (
     <S.Card>
       <S.Titulo>{titulo}</S.Titulo>
@@ -21,12 +42,23 @@ const Tarefa = ({ titulo, prioridade, status, descricao, id }: Props) => {
       <S.Tag parametro="status" status={status}>
         {status}
       </S.Tag>
-      <S.Descricao value={descricao} />
+      <S.Descricao
+        disabled={!estaEditando} // Para que a caixa de texto só seja editavel quando o botão editar for selecionado.
+        value={descricao}
+        onChange={(evento) => setDescricao(evento.target.value)} // onchange para recebimento do valor de setDescricao
+      />
       <S.BarraAcoes>
         {estaEditando ? (
           <>
-            <S.BotaoSalvar>Salvar</S.BotaoSalvar>
-            <S.BtnCancelarRemover onClick={() => setEstaEditando(false)}>
+            <S.BotaoSalvar
+              onClick={() => {
+                dispatch(editar({ titulo, prioridade, status, descricao, id }))
+                setEstaEditando(false)
+              }}
+            >
+              Salvar
+            </S.BotaoSalvar>
+            <S.BtnCancelarRemover onClick={cancelarEdicao}>
               Cancelar
             </S.BtnCancelarRemover>
           </>
